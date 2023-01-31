1257568c61ae0549e1aa15548146549d544e7167
Tauri <tauri.saar@voco.ee>
2023-01-31 11:21:28 +0200

    Created project template files

--- myapp/index.js
+++ myapp/index.js
@@ -1,13 +1,12 @@
-
 const express = require('express')
 const app = express()
 
 const path = require('path')
 
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(__dirname + '/views'));
 app.get('/', function (req, res) {
-    res.render('index')
+    res.render('default')
 })
 
 app.listen(3000)
